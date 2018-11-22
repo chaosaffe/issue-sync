3af309ac4a70ebf98108d6aabfaf023434133742
Jason Murray <jason@chaosaffe.io>
2018-11-23 00:36:48 +0100

    add query builders

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -1,6 +1,9 @@
 package sync
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/chaosaffe/issue-sync/pkg/config"
 	ghClient "github.com/chaosaffe/issue-sync/pkg/github"
 	jClient "github.com/chaosaffe/issue-sync/pkg/jira"
@@ -30,4 +33,49 @@ func Sync(cfg config.Config, ghClient ghClient.GitHubClient, jiraClient jClient.
 func getGitHubIssues(cfg config.Config, ghClient ghClient.GitHubClient) ([]github.Issue, error) {
 
 	return []github.Issue{}, nil
+
+}
+
+func buildQuery(cfg config.Config) (q string) {
+	q += buildUserQuery()
+
+	q += buildOrgQuery(cfg.GetRepos())
+
+	q += buildSinceQuery(cfg.GetSinceParam())
+
+	return q
+}
+
+func buildSinceQuery(since time.Time) (q string) {
+	return fmt.Sprintf("updated:>=%s ", since.Format("2006-01-02T15:04:05+07:00"))
+}
+
+func buildOrgQuery(orgs []config.Organisation) (q string) {
+	for _, org := range orgs {
+		if len(org.Repos) == 0 {
+			q += fmt.Sprintf("org:%s ", org.Name)
+		} else {
+			q += buildRepoQuery(org)
+		}
+	}
+
+	return q
+}
+
+func buildRepoQuery(org config.Organisation) (q string) {
+	for _, repo := range org.Repos {
+		q += fmt.Sprintf("repo:%s/%s ", org.Name, repo)
+	}
+	return q
+}
+
+func buildUserQuery() (q string) {
+	// TODO: get users from gh org
+	users := []string{"chaosaffe", "chrigl", "ainmosni", "syjabri"}
+
+	for _, user := range users {
+		q += fmt.Sprintf("involves:%s ", user)
+	}
+
+	return q
 }
