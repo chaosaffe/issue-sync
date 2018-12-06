0057e78ff7953dbd764e9cf41c3dd86a5db6f543
Jason Murray <jason@chaosaffe.io>
2018-12-06 11:16:22 +0100

    Add basic md->jira conversion package

---
+++ pkg/convert/convert.go
@@ -0,0 +1,34 @@
+package convert
+
+import (
+  "regexp"
+  "strings"
+  )
+
+// This is a go port of github.com/FokkeZB/J2M
+
+func ToJira(markdown string) (out string) {
+  out=markdown
+
+	// remove html comments
+	var comment = regexp.MustCompile(`(?s:<!--.*?-->)`)
+	out = comment.ReplaceAllString(out, "")
+
+  // multi-line comments
+  var multiLineCode = regexp.MustCompile("(?s:`{3}([a-z-]+)?(.*?)`{3})")
+  out = multiLineCode.ReplaceAllString(out, "{code:$1}$2{code}")
+  // fix empty syntax blocks
+  out= strings.Replace(out, "{code:}", "{code}", -1)
+
+  // bold
+  var bold = regexp.MustCompile(`(?s:\*{2}(.*?)\*{2})`)
+  out = bold.ReplaceAllString(out, "*$1*")
+
+
+
+	return out
+}
+
+func ToMD(jira string) string {
+	return jira
+}
